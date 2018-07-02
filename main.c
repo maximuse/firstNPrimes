ce6b1f26daf71d13154c3aba397d70228fa5dc6f
László Kővári <maximuse@windowslive.com>
2018-07-02 18:56:38 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool isPrime(int n) {
+    if(n == 1) {
+        return false;
+    }
+
+    if(n == 2) {
+        return true;
+    }
+
+    for(int i=2; i<n; i++) {
+        if(n%i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    int n;
+    int pieces = 0;
+    int i = 2;
+
+    printf("First n prime numbers: ");
+    scanf("%d", &n);
+
+    printf("\n");
+
+    while(pieces < n) {
+        if(isPrime(i)) {
+            pieces++;
+            printf("%d\n", i);
+        }
+
+        i++;
+    }
+
+    printf("\n");
+
+    system("PAUSE");
+    return 0;
+}
